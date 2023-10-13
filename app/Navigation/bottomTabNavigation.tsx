8cd0cdb4a2d440c57e5330b4a6478dc0e97e6125
Ricardo Omar Lugo Vargas <ricardo.lugo@nuvem.mx>
2023-10-13 17:24:47 -0600

    feature/agregar-iconos | Agregando iconos de bottom tab

--- app/Navigation/bottomTabNavigation.tsx
+++ app/Navigation/bottomTabNavigation.tsx
@@ -8,16 +8,23 @@ const Tab = createMaterialBottomTabNavigator();
 export default function MyTabs() {
   return (
     <Tab.Navigator  initialRouteName="Home"
-    activeColor="#e91e63"
+    activeColor="black"
+    inactiveColor="#fff"
     barStyle={{ backgroundColor: 'tomato' }}>
       <Tab.Screen name="Home" component={Home} options={{
           tabBarLabel: 'Home',
           tabBarIcon: ({ color }) => (
-            <MaterialCommunityIcons name="home" color={color} size={26} />
+            <MaterialCommunityIcons name="home" color={color} size={20} />
           ),
         }}
         />
-      <Tab.Screen name="Details" component={DetailsScreen} />
+      <Tab.Screen name="Details" component={DetailsScreen} options={{
+          tabBarLabel: 'Detalles',
+          tabBarBadge: 3,
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={20} />
+          ),
+        }}/>
     </Tab.Navigator>
   );
 }
